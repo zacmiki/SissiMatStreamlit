f936c47fa7c68f9714b5d05b086f9e01bc729879
zacmiki <95082573+zacmiki@users.noreply.github.com>
2024-05-22 16:04:47 +0200

    Update irconverters.py

--- irconverters.py
+++ irconverters.py
@@ -6,27 +6,46 @@ import streamlit as st
 def page1():
     import pandas as pd
     import numpy as np
-    from sissi_util import DACTemp, DACPress
+    from sissi_util import thz2wn, en2wn, wl2wn, wn2en
         
     # Filter the DataFrame to select the first 20 elements where 'SD' is not NaN
     # --------- PAGE LAYOUT
     
     st.title("🌈️ Infrared Spectroscopy Utils")
-    st.subheader("Useful Infrared Converters")
+    st.subheader("Converters for IR Spectroscopy")
     st.divider()
     
-    st.subheader("1 - Pressure in GPa from Ruby Line Position")
-    #st.text("Pressure in GPa from Ruby Fluorescence Line Position")
+    with st.container():
+        left_column, right_column = st.columns(2)
+        
+        with left_column:
 
-    st.markdown(f"#### :red[Ruby Position in nm] ####")
-    
-    linepos = st.number_input("", step = 0.01 , format="%.2f", value = 694.19)
-    st.markdown(f"#### :red[Corresponding Pressure =]  {DACPress(linepos)} GPa ####")
-    
-    
-    df = pd.DataFrame({
-        'first column': [1,2,3,4],
-        'second column': [10,20,30,40]
-    })
-    df
-    
+            st.markdown("##### :red[THz to cm-1]")
+            thz = st.number_input("", value=1.0, key="thz2wn", label_visibility="collapsed")
+            wn = thz2wn(thz)
+            st.markdown(f"##### {thz} Thz = **{wn:.0f} cm-1**")
+
+            st.markdown("##### :red[Wavelength [nm] to cm-1]")
+            wl = st.number_input("", value=1.0, key="wl2wn", label_visibility="collapsed")
+            wn3 = wl2wn(wl)
+            st.markdown(f"##### {wl} nm = **{wn3:.0f} cm-1**")
+            
+            
+            
+        with right_column:
+            
+            st.markdown("##### :red[meV to cm-1]")
+            ev = st.number_input("", value=1.0, key="ev2wn", label_visibility="collapsed")
+            wn2 = en2wn(ev)
+            st.markdown(f"##### {ev} meV = **{wn2:.0f} cm-1**")
+            
+            st.markdown("##### :red[cm-1 to meV]")
+            wn4 = st.number_input("", value=1.0, key="wn2en", label_visibility="collapsed")
+            en3 = wn2en(wn4)
+            st.markdown(f"##### {wn4} cm-1 = **{en3:.0f} meV**")
+            
+            
+    st.divider()
+    st.header(":rainbow[Cool comments and notes underneath]")
+            
+            
