0432c4a05391a9333217e4b7baa3305a0e9a1566
zacmiki <95082573+zacmiki@users.noreply.github.com>
2024-06-06 17:46:44 +0200

    Update OpusGraher.py

--- OpusGraher.py
+++ OpusGraher.py
@@ -1,6 +1,7 @@
 import streamlit as st
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 import opusFC
+import os
 
 def graphopus():
     st.title("Raw OPUS File Grapher")
@@ -11,6 +12,7 @@ def graphopus():
 
     if uploaded_file is not None:
         file_name = uploaded_file.name
+        st.session_state.fileloaded = file_name
         file_extension = file_name.split(".")[-1]
         
         # Check if the file extension is an integer
@@ -32,22 +34,39 @@ def opusgrapher(file_path):
     data = [opusFC.getOpusData(file_path, item) for item in dbs]
     spectra_names = [num[0] for num in dbs]
     spectra = [{'x': item.x, 'y': item.y} for item in data]
+    st.write(f"The Loaded File is: :red[{st.session_state.fileloaded}]")
     
     data = {name: spec for name, spec in zip(spectra_names, spectra)}
     
-    fig, axs = plt.subplots(len(data), figsize=(10, len(data) * 5))
-    
-    for i, key in enumerate(data):
-        if len(data) == 1:
-            ax = axs
-        else:
-            ax = axs[i]
-        ax.plot(data[key]["x"], data[key]["y"])
-        ax.set_title(key)
-        ax.grid()
+    for key in data:
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=data[key]['x'], y=data[key]['y'], mode='lines', name=key))
+        
+        fig.update_layout(
+            title_text=key,
+            #showlegend=True,
+            
+            margin=dict(l=40, r=40, t=40, b=40),
+            
+            #paper_bgcolor="LightSteelBlue",
+            #plot_bgcolor="white",
+            #yaxis=dict(showgrid=True, gridcolor='LightGray', zeroline=True, zerolinecolor='LightGray',
+            
+            autosize=True,
+            height=400,
+        )
+        fig.update_xaxes(
+            showline = True, linewidth = 2, linecolor = "white", 
+            showgrid=True,
+            mirror = True,
+        )
+        fig.update_yaxes(
+            showline = True, linewidth = 2, linecolor = "white",
+            zeroline = True, zerolinecolor = "brown",
+            showgrid=True,
+            mirror = True,
+        )
+        st.plotly_chart(fig)
 
-    plt.tight_layout()
-    st.pyplot(fig)
-
-
-
+def opusfileexport():
+    return
