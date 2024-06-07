682eb1c5e8ac3147e94c0217a973b1edb590db9f
zacmiki <95082573+zacmiki@users.noreply.github.com>
2024-06-07 16:58:50 +0200

    Update Opusinspector.py

--- Opusinspector.py
+++ Opusinspector.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import opusFC
+from OpusGraher import opusfileexport
 
 def inspectopusfile():
     st.title("OPUS File Inspector")
@@ -24,12 +25,14 @@ def inspectopusfile():
 
 def opusreadinfo(file_path):
     from sissi_util import parvalues
-    
-    dbs = opusFC.listContents(file_path)
-    
+
+    dbs = opusFC.listContents(file_path) 
     dataSets = len(dbs)
     data = [opusFC.getOpusData(file_path, item) for item in dbs]
     spectra_names = [num[0] for num in dbs]
+    
+    if st.button("Press to Download the Spectra as TXT", key = "download"):
+        opusfileexport(file_path)
     
     st.markdown(f"### :blue[There are {dataSets} datasets inside the file]")
     st.markdown(f"##### {dbs}")
@@ -38,5 +41,4 @@ def opusreadinfo(file_path):
     #spectra = [...... for item in data]
     #data = {name: spec for name, spec in zip(spectra_names, spectra)}
     st.divider()
-    
     parvalues(file_path) 
